1c0449725cfc07bc2014d7a7ffeb37252f5c4cf1
almazko <a.s.suslov@gmail.com>
2014-10-25 14:17:51 +0400

    #1 add dummy for gyms

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@
  * Time: 12:34
  */
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
@@ -15,6 +15,18 @@ $app['debug'] = true;
 
 $app->get('/', function () use ($app) {
     return '<h1>HackFitness</h1>';
+});
+
+
+$app->get('/gyms', function () use ($app) {
+
+    $id = 1;
+    $gyms = [];
+    for ($i = 1; $i < 20; $i++) {
+        $gyms[] = ['id' => $id++, 'name' => "Gym $id", 'logo' => 'i/gym_stub.png'];
+    }
+
+    return $app->json($gyms);
 });
 
 $app->run();
