0052936623722de0f80e570c0447928992fa6487
almazko <a.s.suslov@gmail.com>
2014-10-25 21:38:26 +0400

    #4 add stub trainers

--- web/index.php
+++ web/index.php
@@ -43,16 +43,31 @@ $app->get('/gyms', function () use ($app) {
 
     return $app->json($gyms);
 });
-$app->get('/gym_addresses/{gym_id}', function ($gymId=0) use ($app) {
+
+
+$app->get('/gym_addresses/{gym_id}', function ($gymId = 0) use ($app) {
 
     $id = 0;
-    $gyms[] = ['id' => $id++, 'subway' => "Бирюлево", 'address' => 'ул. Михневская, д.4'];
-    $gyms[] = ['id' => $id++, 'subway' => 'Тульская', 'address' => 'ул. Большая Тульская, д.13'];
-    $gyms[] = ['id' => $id++, 'subway' => 'Коломенское', 'address' => 'ул.Андропова, д.22'];
-    $gyms[] = ['id' => $id++, 'subway' => 'Тушино', 'address' => 'ул.Тушинская, д.17'];
-    $gyms[] = ['id' => $id++, 'subway' => 'Юго-западная', 'address' => 'ул.26-ти Бакинских Комиссаров'];
-    $gyms[] = ['id' => $id++, 'subway' => 'Ясенево', 'address' => 'ул.Рокотова, д.5'];
-    
-    return $app->json($gyms);
+    $clubs[] = ['id' => $id++, 'subway' => "Бирюлево", 'address' => 'ул. Михневская, д.4'];
+    $clubs[] = ['id' => $id++, 'subway' => 'Тульская', 'address' => 'ул. Большая Тульская, д.13'];
+    $clubs[] = ['id' => $id++, 'subway' => 'Коломенское', 'address' => 'ул.Андропова, д.22'];
+    $clubs[] = ['id' => $id++, 'subway' => 'Тушино', 'address' => 'ул.Тушинская, д.17'];
+    $clubs[] = ['id' => $id++, 'subway' => 'Юго-западная', 'address' => 'ул.26-ти Бакинских Комиссаров'];
+    $clubs[] = ['id' => $id++, 'subway' => 'Ясенево', 'address' => 'ул.Рокотова, д.5'];
+
+    return $app->json($clubs);
 });
+
+
+$app->get('/gym/{gym_id}/{club_id}/trainers', function ($gymId = 0, $clubId = 0) use ($app) {
+
+    $id = 0;
+    $trainers[] = ['id' => $id++, 'name' => "Кирил Агапов", 'photo' => 'i/trainer_stub.jpg'];
+    $trainers[] = ['id' => $id++, 'name' => "Лариса Иванова", 'photo' => 'i/trainer_stub.jpg'];
+    $trainers[] = ['id' => $id++, 'name' => "Константин Крутов", 'photo' => 'i/trainer_stub.jpg'];
+
+    return $app->json($trainers);
+});
+
+
 $app->run();
